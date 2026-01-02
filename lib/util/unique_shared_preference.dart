710acca8fbcdae242d0549e1b8938f308c5794b9
c3110y3110 <145450310+c3110y3110@users.noreply.github.com>
2026-01-02 18:18:47 +0900

    Change setString and init methods to async

--- lib/util/unique_shared_preference.dart
+++ lib/util/unique_shared_preference.dart
@@ -4,14 +4,14 @@ import 'package:tsr_monitoring_app/util/constants.dart';
 class UniqueSharedPreference {
   static late final SharedPreferences _instance;
 
-  static void init() async {
+  static Future<void> init() async {
     _instance = await SharedPreferences.getInstance();
-    setString('selectedIndex', '0');
-    setString('selectedUnit', avgList[0]);
-    setString('maxvalue', '10.0');
-    setString('minvalue', '-10.0');
+    await setString('selectedIndex', '0');
+    await setString('selectedUnit', avgList[0]);
+    await setString('maxvalue', '10.0');
+    await setString('minvalue', '-10.0');
     if (!_instance.containsKey('selectedMachines')) {
-      setStringList('selectedMachines', List<String>.from(machineList));
+      await setStringList('selectedMachines', List<String>.from(machineList));
     }
   }
 
@@ -19,7 +19,7 @@ class UniqueSharedPreference {
     return _instance.getString(key) ?? defValue ?? "";
   }
 
-  static void setString(String key, String value) async {
+  static Future<void> setString(String key, String value) async {
     var prefs = await _instance;
     prefs.setString(key, value);
     await prefs.commit();
@@ -29,7 +29,7 @@ class UniqueSharedPreference {
     return _instance.getStringList(key) ?? defValue ?? <String>[];
   }
 
-  static void setStringList(String key, List<String> value) async {
+  static Future<void> setStringList(String key, List<String> value) async {
     var prefs = await _instance;
     prefs.setStringList(key, value);
     await prefs.commit();
